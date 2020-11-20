6b46d353c0f57b2961e3b658c2e234c8c37692d9
narumol01 <73689289+narumol01@users.noreply.github.com>
2020-11-20 14:34:38 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 echo"<h2>TEST</h2>";
+echo"<TABLE width = "50%" height = "60%" >";
+echo"<CAPTION> ขนาดของตาราง </CAPTION>";
+echo"<TR> <TH> หัวเรื่อง 1 </TH>";
+echo"<TH> หัวเรื่อง 2 </TH> </TR>";
+echo"<TR> <TD> ข้อมูล 1 </TD>";
+echo"<TD> ข้อมูล 2 </TD> </TR>";
+echo"</TABLE>";
 ?>
